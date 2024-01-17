dc26fcb72c63e0a158ee205bd527f424a0c87a0d
Jayesh Chauhan <jayesh.chauhan@cubastion.com>
2024-01-17 14:05:19 +0530

    updated classes page with changes in db.js

--- src/Pages/Classes.js
+++ src/Pages/Classes.js
@@ -5,34 +5,46 @@ import { useLoaderData } from "react-router-dom";
 
 export const loader = async () => {
   const res = await customFetch.get("/class");
-  return { batches: res.data };
+  const response = await customFetch.get("/student");
+  return { batches: res.data, students:response.data };
 };
 
 const Classes = () => {
-  const { batches } = useLoaderData();
+  const { batches,students } = useLoaderData();
+  console.log(batches);
 
   return (
     <div className="container mt-4">
       <h2>Classes</h2>
       <div className="row">
-        {batches.map((batch) => (
-          <div key={batch.id} className="col-md-4 mb-4">
+        {batches.map((batch)=>{
+          const {id,name,courseID,classId} = batch;
+
+          console.log(classId);
+          const classStudents = students.filter((student)=>student.classId === classId);
+          console.log(classStudents);
+
+          return(
+            <>
+             <div key={id} className="col-md-4 mb-4">
             <Link
-              to={`/classes/${batch.id}`}
+              to={`/classes/${id}`}
               style={{ textDecoration: "none", color: "inherit" }}
             > 
               <div className="card">
                 <div className="card-body">
-                  <h5 className="card-title">{batch.name}</h5>
-                  <p className="card-text">Courses: {batch.courseIDs.length}</p>
+                  <h5 className="card-title">{name}</h5>
+                  <p className="card-text">Courses: {courseID.length}</p>
                   <p className="card-text">
-                    Students: {batch.studentIDs.length}
+                    Students: {classStudents?.length}
                   </p>
                 </div>
               </div>
             </Link>
           </div>
-        ))}
+            </>
+          )
+        })}
       </div>
     </div>
   );
