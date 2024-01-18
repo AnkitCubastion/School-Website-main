8a9320ec1448693b3aa4454929de99de4f06608d
reshabh27 <reshabhsahu10@gmail.com>
2024-01-18 16:16:17 +0530

    added single student and student page also landing carousel

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Founder from "../Component/Founder";
+import { LandingCarousel } from "../Component/LandingCarousel";
 
 const Landing = () => {
   return (
     <>
+      <LandingCarousel/>
       <Founder />
+      
     </>
   );
 };
